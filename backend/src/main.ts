1c8480479c7c557cddac5b3b882f8b23d47a69c2
Kosyakov Egor <58385866+ra4nd0m@users.noreply.github.com>
2025-10-06 22:31:52 +0300

    fix: update default PORT value from 3000 to 8081

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,7 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
-const { PORT = 3000 } = process.env;
+const { PORT = 8081 } = process.env;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
